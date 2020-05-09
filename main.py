26c5e24e8ade3a235b5981ce35eb62de43e83360
Sangeetha Shanmugam <sansha17@gmail.com>
2020-05-09 01:17:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+
+""""
+list = ["sang", "jay", "sush", 1]
+
+print(list)
+
+
+list.reverse()
+
+print(list)
+"""""
+
+
+### Slicing, Striding, for Loop
+
+#list = [1, 2, 3, 4, 5 , 6, 7, 8, 9]
+
+#print(list)
+
+#print (list[8::-2])
+
+"""
+window_size = 4
+for i in range(len(list) - (window_size-1)):
+  print(list[i:i+window_size])
+
+
+"""
+
+
+
+
+
